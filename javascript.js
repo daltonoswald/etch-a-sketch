6054d2181ed7d42273112a5c2b31ccf668183169
daltonoswald <daltonoswald@gmail.com>
2023-06-07 18:48:52 -0400

    created functions to user set sizes and fixed size of squares

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
-const columns = 16;
-const rows = 16;
+function makeGrid(rows, columns){
+const container = document.querySelector('.container');
 
 const grid = document.createElement('div');
 grid.className = 'grid';
@@ -9,12 +9,26 @@ for (let i = 0; i < columns; i++) {
     for (let j = 0; j < rows; j++) {
         const row = document.createElement('div');
         row.className = 'row';
-        row.textcontent = "test";
         column.appendChild(row);
     }
     grid.appendChild(column);
-    grid.addEventListener('mouseover', (e) => {
+    column.addEventListener('mouseover', (e) => {
         e.target.style.background = 'black';
     });
 }
-document.body.appendChild(grid);
+container.appendChild(grid);
+}
+makeGrid(5,5);
+
+function newReset() {
+    let userPrompt = prompt("Please enter the amount of squares per side", 5);
+    const container = document.querySelector('.container');
+    container.innerHTML = "";
+    if (isNaN(userPrompt) || userPrompt > 100 || userPrompt <= 0) {
+        alert("You must enter a number between 1 and 100");
+        container.innerHTML = "";
+        makeGrid(5,5);
+    } else {
+    makeGrid(userPrompt, userPrompt);
+    };
+};
